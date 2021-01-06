fe777d09fd2c32536283c8d69c29f07863bb8ac8
Timo Göckeler <timo.goeckeler@gmail.com>
2021-01-06 19:12:12 +0100

    Erste kurze Version der zufallsgenerierten Begrüßung

--- Randomized Greetings/RandomizedGreetings.java
+++ Randomized Greetings/RandomizedGreetings.java
@@ -4,7 +4,18 @@ public class RandomizedGreetings
 
     public RandomizedGreetings()
     {
-
+        int random=Integer.parseInt(String.valueOf(Math.round(Math.random()*1)));
+        start(random);      
+        }
+    
+private void start(int random)
+{
+    if(random==0)
+    {
+        System.out.println("Hallo Timo");
+    }else
+    {
+        System.out.println("Hallo Thorsten");
     }
-
+}
 }
